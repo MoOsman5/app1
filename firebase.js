0aebe9cc1dd3b01fa781390327e7be54e3f6510f
MoOsman5 <mohamed1592000gmail.com>
2023-03-24 15:09:02 +0200

    import firestore

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +23,6 @@ if(firebase.apps.length=== 0){
 }
 
 const auth = firebase.auth();
+const db =firebase.firestore();
 
-export {auth};
+export {auth,db};
